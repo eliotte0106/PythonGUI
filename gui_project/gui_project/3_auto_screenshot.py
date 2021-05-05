1e5fa67b7ae68c216e6b19771588b11ee2c93111
Eliotte Park <jihong2253@daum.net>
2021-05-04 20:35:20 -0400

    Delete gui_project/gui_project directory

--- gui_project/gui_project/3_auto_screenshot.py
+++
@@ -1,9 +0,0 @@
-import time
-from PIL import ImageGrab
-
-time.sleep(5) # wait 5
-
-for i in range(1,11):#every 2 sec, save 10 images
-    img.ImageGrab.grab()#current screen 
-    img.save("image{}.png".format(i)) #save as file
-    time.sleep(2)
